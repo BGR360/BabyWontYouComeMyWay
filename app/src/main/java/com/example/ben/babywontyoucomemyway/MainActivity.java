cb7f2fe128f163bd620d4109489f859b8b807cce
Ben Reeves <benwolverine2019@gmail.com>
2015-10-08 20:39:21 -0400

    Allowed user to use volume buttons to control playback volume

--- app/src/main/java/com/example/ben/babywontyoucomemyway/MainActivity.java
+++ app/src/main/java/com/example/ben/babywontyoucomemyway/MainActivity.java
@@ -1,20 +1,15 @@
 package com.example.ben.babywontyoucomemyway;
 
-import android.graphics.PorterDuff;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.support.annotation.IdRes;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener,
                                                                MediaPlayer.OnPreparedListener
@@ -71,6 +66,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         wantToSayPlayer = new SoundPlayer(this, R.raw.want_to_say, this);
         spawPlayer = new SoundPlayer(this, R.raw.spaw, this);
         gawdamPlayer = new SoundPlayer(this, R.raw.gawdam_gawdam, this);
+
+        // Allow user to use volume buttons to control playback volume
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
     }
 
     @Override
